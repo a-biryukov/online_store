e1676bd4accf0813ca728ee1d58a0ce162bbd93b
Alexander <a.biryukov164@gmail.com>
2024-02-12 18:35:35 +0400

    add test for class Catecory

---
+++ tests/test_classes.py
@@ -0,0 +1,12 @@
+import pytest
+from src.classes import Category
+
+
+@pytest.fixture()
+def category_smartphone():
+    return Category("Смартфоны", "Многофунциональное устройство", ["Samsung", "IPhane", "Xiaomi"])
+
+def test_init():
+    assert category_smartphone.name == "Смартфоны"
+    assert category_smartphone.description == "Многофунциональное устройство"
+    assert category_smartphone.goods == ["Samsung", "IPhane", "Xiaomi"]
