def823787473a08b697466dea0462f1fdf0bafe7
Alexander <a.biryukov164@gmail.com>
2024-02-12 18:39:43 +0400

    init class Catecory

---
+++ src/classes.py
@@ -0,0 +1,6 @@
+class Category:
+
+    def __init__(self, name, description, goods):
+        self.name = name
+        self.description = description
+        self.goods = goods
--- tests/test_classes.py
+++ tests/test_classes.py
@@ -6,7 +6,8 @@ from src.classes import Category
 def category_smartphone():
     return Category("Смартфоны", "Многофунциональное устройство", ["Samsung", "IPhane", "Xiaomi"])
 
-def test_init():
+
+def test_init(category_smartphone):
     assert category_smartphone.name == "Смартфоны"
     assert category_smartphone.description == "Многофунциональное устройство"
     assert category_smartphone.goods == ["Samsung", "IPhane", "Xiaomi"]
