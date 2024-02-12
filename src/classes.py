ac7a2f6533d09e0246dff35ebbdf6d2879849ae8
Alexander <a.biryukov164@gmail.com>
2024-02-12 21:19:05 +0400

    add docstrings

--- src/classes.py
+++ src/classes.py
@@ -1,17 +1,38 @@
 class Base:
-    def __init__(self, name, description):
+    """Базовый класс для наследования"""
+    def __init__(self, name: str, description: str) -> None:
+        """
+        Метод для инициализации экземпляра класса. Задаем значения атрибутам экземпляра.
+        :param name: Название
+        :param description: Описание
+        """
         self.name = name
         self.description = description
 
 
 class Category(Base):
-    def __init__(self, name, description, products):
+    """Класс для представления категорий товаров"""
+    def __init__(self, name: str, description: str, products: list) -> None:
+        """
+        Метод для инициализации экземпляра класса. Задаем значения атрибутам экземпляра.
+        :param name: Название категории товаров
+        :param description: Описание категории товаров
+        :param products: Список товаров
+        """
         super().__init__(name, description)
         self.products = products
 
 
 class Product(Base):
-    def __init__(self, name, description, price, quantity):
+    """Класс для представления товаров"""
+    def __init__(self, name: str, description: str, price: float, quantity: int) -> None:
+        """
+        Метод для инициализации экземпляра класса. Задаем значения атрибутам экземпляра.
+        :param name: Название товара
+        :param description: Описание товара
+        :param price: Цена товара
+        :param quantity: Количество данного товара на складе
+        """
         super().__init__(name, description)
         self.price = price
         self.quantity = quantity
