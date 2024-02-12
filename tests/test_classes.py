eea527874696c912d1c4361cf3556e0464a1fc4a
Alexander <a.biryukov164@gmail.com>
2024-02-12 19:30:32 +0400

    add test for class Product

--- tests/test_classes.py
+++ tests/test_classes.py
@@ -4,10 +4,22 @@ from src.classes import Category
 
 @pytest.fixture()
 def category_smartphone():
-    return Category("Смартфоны", "Многофунциональное устройство", ["Samsung", "IPhane", "Xiaomi"])
+    return Category("Смартфоны", "Многофунциональное устройство", ["Samsung", "IPhone", "Xiaomi"])
 
 
-def test_init(category_smartphone):
+@pytest.fixture()
+def product_iphone():
+    return Product("IPone", "512GB, Gray space", 210000.0, 8)
+
+
+def test_init_category(category_smartphone):
     assert category_smartphone.name == "Смартфоны"
     assert category_smartphone.description == "Многофунциональное устройство"
     assert category_smartphone.goods == ["Samsung", "IPhane", "Xiaomi"]
+
+
+def test_init_product(product_iphone):
+    assert product_iphone.name == "IPone"
+    assert product_iphone.description == "512GB, Gray space"
+    assert product_iphone.price == 210000.0
+    assert product_iphone.quantity == 8
